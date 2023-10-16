db156ef89a5f59f9c216d5f5a537f1446598db4d
CurioShade <dawid.rp035@gmail.com>
2023-10-16 22:24:06 +0200

    Move page list to pager partial

--- app/controllers/concerns/paginator.rb
+++ app/controllers/concerns/paginator.rb
@@ -1,8 +1,9 @@
 # Controller concern which adds simple variables for pagination
 module Paginator
     extend ActiveSupport::Concern
+
     def set_pagination
-        @page_limit = params.has_key?(:record_limit) ? params[:record_limit] : 5
-        @record_id = params.has_key?(:start) ? params[:start] : 1
+        @page_limit = params.has_key?(:record_limit) ? params[:record_limit].to_i : 5
+        @record_id = params.has_key?(:start) ? params[:start].to_i : 1
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    # Returns string representing url path without query parameters
+    def url_clean_path
+        request.fullpath.sub("?" + request.query_string, "")
+    end
 end
