7739aca3914c4da3b8bba3b767b4fee754039fb8
CurioShade <dawid.rp035@gmail.com>
2023-10-20 18:48:49 +0200

    Introduce BCrypt to the project

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
 
+# Provide bcrypt for password encryption
+gem 'bcrypt', '~> 3.1.7', '>= 3.1.12'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.0"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_08_091152) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_20_154300) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_08_091152) do
     t.integer "access_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
