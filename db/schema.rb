fb114b261b4cd7f2ab48b526320d993cd0ea13f1
CurioShade <dawid.rp035@gmail.com>
2023-10-20 22:59:44 +0200

    Further changes to incorporate BCrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_20_154300) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_20_203254) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_20_154300) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.integer "access_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
