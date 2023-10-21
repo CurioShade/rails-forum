8f6863c46c47a628b781f86c9498a079e62e038e
CurioShade <dawid.rp035@gmail.com>
2023-10-21 15:24:06 +0200

    Fixes unintentional redirection when there aren't any records

--- app/controllers/concerns/paginator.rb
+++ app/controllers/concerns/paginator.rb
@@ -6,8 +6,12 @@ module Paginator
         @page_limit = record_limit
         @page = params.has_key?(:page) ? params[:page].to_i : 1
         @record_id = model[@page_limit * (@page-1)]&.id
+        max_pages = model.max_pages(@page_limit)
+        unless max_pages > 0
+            max_pages = 1
+        end 
 
-        if @page < 1 || @page > model.max_pages(@page_limit)
+        if @page < 1 || @page > max_pages
             redirect_back_or_to url_clean_path
         end
     end
