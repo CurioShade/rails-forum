f157daaea4f1d8d17066341c4f60f859eeefbf29
CurioShade <dawid.rp035@gmail.com>
2023-12-25 17:38:28 +0100

    Adds category destruction for admin to use.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,17 @@ class CategoriesController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        if @category.destroyed?
+            respond_to do |format|
+                format.html { redirect_to root_url }
+                format.turbo_stream { render turbo_stream: turbo_stream.remove(@category) }
+            end
+        end
+    end
     
     private
     def category_params
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
 module CategoriesHelper
+    def category_destroy_button(category)
+        button_to category_path(category), method: :delete, 
+        class: "exit-button" do
+            image_tag "exit.svg", size: "14x14"
+        end
+    end
 end
